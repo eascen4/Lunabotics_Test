563a8129cac47990cd361442ca95b637da2ba2c8
eascen4 <eascen4official@gmail.com>
2024-11-11 19:41:30 -0600

    Added x and y for frames

--- lunabotics_package/lunabotics_package/camera_node.py
+++ lunabotics_package/lunabotics_package/camera_node.py
@@ -28,9 +28,8 @@ class CameraNode(Node):
         try:
             self.pipeline = rs.pipeline()
             config = rs.config()
-            config.enable_stream(rs.stream.color, rs.format.bgr8, 30)
-            config.enable_stream(rs.stream.depth, rs.format.z16, 30)
-            config.enable_stream(rs.stream.gyro)
+            config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+            config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
             self.pipeline.start(config)
 
             self.timer = self.create_timer(0.1, self.camera_callback)
