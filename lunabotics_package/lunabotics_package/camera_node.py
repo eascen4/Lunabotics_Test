3af4487c259889a9149f5059b8775647b9823047
eascen4 <eascen4official@gmail.com>
2024-11-12 19:10:57 -0600

    added gyro y to message

--- lunabotics_package/lunabotics_package/camera_node.py
+++ lunabotics_package/lunabotics_package/camera_node.py
@@ -15,14 +15,11 @@ class CameraNode(Node):
         super().__init__('camera_node')
         self.get_logger().info('Camera Node Started')
         self.msg_count = 0
+
         self.camera_width = 640
         self.camera_height = 480
 
-        self.angle_min = -0.5
-        self.angle_max = 0.5
-        self.angle_increment = (self.angle_max - self.angle_min) / self.camera_width
-        self.range_min = 0.1
-        self.range_max = 10.0
+        self.depth_horizontal_fov = math.radians(87)
 
         try:
             self.pipeline = rs.pipeline()
@@ -47,13 +44,15 @@ class CameraNode(Node):
 
         color_frame = frames.get_color_frame()
         depth_frame = frames.get_depth_frame()
+        gyro_frame = frames.first_or_default(rs.stream.gyro)
 
         if not color_frame or not depth_frame:
+            self.get_logger().warning("Frames not received")
             return
 
         depth_data = np.asanyarray(depth_frame.get_data())
-        gyro_data = frames[2].as_motion_frame().get_motion_data()
-        
+        gyro_data = gyro_frame.as_motion_frame().get_motion_data()
+        angle = gyro_data.y
 
         message = LaserScan()
 
@@ -63,9 +62,9 @@ class CameraNode(Node):
         message.header.stamp = self.get_clock().now().to_msg()
 
         # Set LaserScan message parameters
-        message.angle_min = -math.pi / 4 # TODO: make non-hardcoded
-        message.angle_max = math.pi / 4 # TODO: make non-hardcoded
-        message.angle_increment = (math.pi / 2) / depth_frame.get_width() # The D455 has a horizontal FOV of 90 degrees
+        message.angle_min = -self.depth_horizontal_fov / 2 + angle 
+        message.angle_max = self.depth_horizontal_fov / 2 + angle 
+        message.angle_increment = self.depth_horizontal_fov / self.camera_width # The D455 has a horizontal FOV of 90 degrees
 
         message.time_increment = 0.0 # D455 takes all scans at once
         message.scan_time = 1/30 # 30Hz (time between scans)
@@ -81,7 +80,7 @@ class CameraNode(Node):
 
         self.scan_publisher.publish(message)
         self.msg_count += 1
-        self.get_logger().info(f"LaserScan message #{self.msg_count} published at angle {gyro_data}")
+        self.get_logger().info(f"LaserScan message #{self.msg_count} published at angle {angle}")
 
 
     def stop_pipeline(self):
